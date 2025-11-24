b4f4a01218d8f399b55a9895799d9a043b8a972a
SamruddhiGhodke <137768747+SamruddhiGhodke@users.noreply.github.com>
2025-11-24 15:44:35 +0530

    first commit

---
+++ backend/handel/src/main/java/com/im/handel/HandelApplication.java
@@ -0,0 +1,14 @@
+package com.im.handel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class HandelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandelApplication.class, args);
+	}
+
+}
