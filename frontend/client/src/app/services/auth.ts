25b86d765ce3ab6c034c7c8fef8988b5c8073a26
SamruddhiGhodke <137768747+SamruddhiGhodke@users.noreply.github.com>
2025-11-25 17:17:01 +0530

    docker compose

--- frontend/client/src/app/services/auth.ts
+++ frontend/client/src/app/services/auth.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class AuthService {
 
-  private apiUrl = 'https://localhost/login'; // change to your API
+  private apiUrl = 'http://localhost:8080/admin/login'; // change to your API
 
   constructor(private http: HttpClient) {}
 
