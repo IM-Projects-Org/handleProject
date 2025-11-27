a10d76d2d5e795d132e61611a72e225eff718a37
nithyanand9969 <nithyanandnadar.m@gmail.com>
2025-11-27 19:17:53 +0530

    docker_composer/added/v2

--- client/src/app/components/nav/nav.ts
+++ client/src/app/components/nav/nav.ts
@@ -1,14 +1,47 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { Router, RouterModule } from '@angular/router';
+import { Router, RouterModule, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-nav',
-  imports: [CommonModule,RouterModule],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './nav.html',
-  styleUrl: './nav.css',
+  styleUrl: './nav.css'
 })
 export class Nav {
 
+  role = '';
+  isLoggedIn = false;
+  hideNav = false;
+
   constructor(public router: Router) {}
+
+  ngOnInit() {
+    // Check on page load
+    this.checkNavbarState();
+
+    // Check on every route change
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => this.checkNavbarState());
+  }
+
+checkNavbarState() {
+  const url = this.router.url.split('?')[0].split('#')[0];  // clean URL
+
+  // Hide navbar ONLY on login & register
+  this.hideNav = (url === '/login' || url === '/register');
+
+  // Check login status
+  this.isLoggedIn = !!localStorage.getItem('token');
+  this.role = localStorage.getItem('role') || '';
+}
+
+
+  logout() {
+    localStorage.clear();
+    this.router.navigate(['/login']);
+  }
 }
